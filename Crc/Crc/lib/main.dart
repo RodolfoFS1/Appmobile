172cc73fa4845f6e4ed82b8d8b2bafa570b1dae7
RodolfoFS1 <150473503+RodolfoFS1@users.noreply.github.com>
2024-11-15 13:35:54 -0600

    App mobile
    Aplicación móvil para registro de solicitantes

---
+++ Crc/Crc/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'screens/login.dart';
+import 'screens/homeScreen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Login(
+        onLogin: () {
+          // El evento de login se maneja dentro de la pantalla de Login.
+        },
+      ),
+    );
+  }
+}
